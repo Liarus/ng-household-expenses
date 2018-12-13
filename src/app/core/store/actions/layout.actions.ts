36f5b195d14a022b963462b573b6271f9edb530d
Tomasz Hodorek <tomaszh@dip.co.uk>
2018-12-13 19:19:22 +0100

    layout actions

---
+++ src/app/core/store/actions/layout.actions.ts
@@ -0,0 +1,28 @@
+import { Action } from '@ngrx/store';
+
+import { WindowSize } from '../../models/windowSize.model';
+
+export enum LayoutActionTypes {
+    OpenSidebar = '[Layout] Open Sidebar',
+    CloseSidebar = '[Layout] Close Sidebar',
+    ResizeWindow = '[Layout API] Resize window'
+}
+
+export class OpenSidebar implements Action {
+    readonly type = LayoutActionTypes.OpenSidebar;
+}
+
+export class CloseSidebar implements Action {
+    readonly type = LayoutActionTypes.CloseSidebar;
+}
+
+export class ResizeWindow implements Action {
+    readonly type = LayoutActionTypes.ResizeWindow;
+
+    constructor(public payload: WindowSize) {}
+}
+
+export type LayoutActionsUnion =
+    | OpenSidebar
+    | CloseSidebar
+    | ResizeWindow;
