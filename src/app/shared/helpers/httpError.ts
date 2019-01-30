05a7c9b9a3671f4f7f9bdaed93f99abbc03c5c32
Tomasz Hodorek <tomaszh@dip.co.uk>
2019-01-30 08:59:13 +0100

    additonal property to check

--- src/app/shared/helpers/httpError.ts
+++ src/app/shared/helpers/httpError.ts
@@ -13,6 +13,10 @@ export class HttpError {
             errorMessage.message = error.message;
             return errorMessage;
         }
+        if (error.statusText) {
+            errorMessage.message = `error occured: ${error.statusText}`;
+            return errorMessage;
+        }
         if (error.status) {
             errorMessage.message = `error occured with status: ${error.status}`;
             return errorMessage;
